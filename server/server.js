556aa06e25c634f18ab0172b25cc6bf49025de9f
Charlotte Rutland <charlierutland@gmail.com>
2018-11-17 19:16:01 +0100

    ongo server

--- server/server.js
+++ server/server.js
@@ -4,22 +4,22 @@ const cors = require('cors');
 const controller = require('./controller');
 
 app
-	.use(cors())
-	.get('/ads', (req, res) => {
-		controller.getAllAds(req, res);
-	})
-	.get('/ads/:id', (req, res) => {
-		controller.getAd(req, res);
-	})
-	.use(express.json())
-	.post('/ads', (req, res) => {
-		controller.createAd(req, res);
-	})
-	.delete('/ads/:id', (req, res) => {
-		controller.deleteAd(req, res);
-	})
-	.use((_, res) => {
-		res.status(404).end();
-	});
+  .use(cors())
+  .get('/ads', (req, res) => {
+    controller.getAllAds(req, res);
+  })
+  .get('/ads/:id', (req, res) => {
+    controller.getAd(req, res);
+  })
+  .use(express.json())
+  .post('/ads', (req, res) => {
+    controller.createAd(req, res);
+  })
+  .delete('/ads/:id', (req, res) => {
+    controller.deleteAd(req, res);
+  })
+  .use((_, res) => {
+    res.status(404).end();
+  });
 
 module.exports = app;
