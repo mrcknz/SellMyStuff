8c6fd0f9e410934d2f99284a557536d119303a86
Marco Kunz <kunz.marco@gmail.com>
2018-11-20 13:47:28 +0100

    fix (server): add index to ad title

--- server/model.js
+++ server/model.js
@@ -1,27 +1,24 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb://localhost/ads',
-  { useNewUrlParser: true }
-);
+mongoose.connect('mongodb://localhost/ads', { useNewUrlParser: true });
 const adsModel = {};
 
 adsModel.Ad = mongoose.model('Ad', {
-  title: String,
-  price: Number,
-  description: String,
-  image: String,
-  length: Number,
-  width: Number,
-  height: Number,
-  Weight: Number,
-  houseNumber: Number,
-  street: String,
-  postcode: String,
-  city: String,
-  country: String,
-  lat: Number,
-  long: Number
+  title       : { type: String, text: true },
+  price       : Number,
+  description : String,
+  image       : String,
+  length      : Number,
+  width       : Number,
+  height      : Number,
+  Weight      : Number,
+  houseNumber : Number,
+  street      : String,
+  postcode    : String,
+  city        : String,
+  country     : String,
+  lat         : Number,
+  long        : Number
 });
 
 adsModel.getAll = async function() {
@@ -29,10 +26,9 @@ adsModel.getAll = async function() {
 };
 
 adsModel.search = async function(searchTerm) {
-  // adsModel.Ad.createIndex({ title: 'text', description: 'text' });
-  const q = await adsModel.Ad.find({ $text: { $search: searchTerm } }).exec();
-  console.log('test', q);
-  return q;
+  const res = await adsModel.Ad.find({ $text: { $search: searchTerm } }).exec();
+  console.log('test', res);
+  return res;
 };
 
 adsModel.getAd = async function(id) {
@@ -41,21 +37,21 @@ adsModel.getAd = async function(id) {
 
 adsModel.create = async function(ad) {
   const newAd = {
-    title: '',
-    price: '',
-    description: '',
-    image: '',
-    length: '',
-    width: '',
-    height: '',
-    Weight: '',
-    houseNumber: '',
-    street: '',
-    postcode: '',
-    city: '',
-    country: '',
-    lat: '',
-    long: ''
+    title       : '',
+    price       : '',
+    description : '',
+    image       : '',
+    length      : '',
+    width       : '',
+    height      : '',
+    Weight      : '',
+    houseNumber : '',
+    street      : '',
+    postcode    : '',
+    city        : '',
+    country     : '',
+    lat         : '',
+    long        : ''
   };
   return await adsModel.Ad.create(Object.assign(newAd, ad));
 };
